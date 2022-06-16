a53e44fee35211026af5cc4e46d8d456aa0d81dc
GrethelRivera <grethelr91@gmail.com>
2022-06-16 09:04:27 -0400

    first commit

---
+++ getfirstelement.js
@@ -0,0 +1,4 @@
+const array = [ 1, 2, 3, 4, 5 ]; //makes the array
+const firstElement = array[0] //returns the first element 
+
+console.log(firstElement)
---
+++ returnsomething.js
@@ -0,0 +1,6 @@
+function convert(minutes) {
+	return minutes * 60;
+
+}
+
+//Write a function that takes an integer minutes and converts it to seconds.
