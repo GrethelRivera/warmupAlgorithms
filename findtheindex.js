184fa5b7fc6a8d3bfc73b3b32c6b73627aec24e5
GrethelRivera <grethelr91@gmail.com>
2022-06-17 23:34:57 -0400

    first commit

---
+++ findtheindex.js
@@ -0,0 +1,9 @@
+const NumberArray = [1, 2, 3, 4]
+
+console.log(NumberArray.indexOf(3))
+
+//expected output 1
+
+
+// Create a function that searches for the index of a given item in an array.
+//  If the item is present, it should return the index, otherwise, it should return -1.
---
+++ nextnumber.js
@@ -0,0 +1,3 @@
+const x = 22  //Enter number
+
+console.log ( x + 1 ) // Adds 1 to the number entered
