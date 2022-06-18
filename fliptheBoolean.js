d61d366bd533f43d149b7bd5e4f32ec91e53f910
GrethelRivera <grethelr91@gmail.com>
2022-06-17 23:33:52 -0400

    first commit

---
+++ fliptheBoolean.js
@@ -0,0 +1,11 @@
+var thing = true; //try changing this to false
+thing = !thing; //the variable will go from true to false, or from false to true
+
+
+
+// Create a function that reverses a boolean value.
+
+// Examples
+// reverse(true) ➞ false
+
+// reverse(false) ➞ true
